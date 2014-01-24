9e18ae6d84d84163afe316f3c86c45cda8f9c2c8
Artur Ostrega <skoofoo@gmail.com>
2014-01-23 19:28:30 -0600

    1st

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OpalLearningWebGL::Application.config.secret_key_base = 'a07f3a0992329284a0b68e490d4a44246a0c44613dd7be8e877263ee4c06e54d7c7a49d2524a21f52fb640dbbc47cf6c36a1560e40aa9fbe00bee14a3238df01'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OpalLearningWebGL::Application.config.session_store :cookie_store, key: '_OpalLearningWebGL_session'
